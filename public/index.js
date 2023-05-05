07c264b66f27f16d5d8f954d3280f151fc157608
NovaSagittarii <novasagittarii@gmail.com>
2023-05-05 15:57:43 -0600

    webrtc communication with phone scanning peripheral

--- public/index.js
+++ public/index.js
@@ -24,6 +24,15 @@ const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
 
+const qrcode = new QRCode(document.getElementById("qrcode"), {
+  width: 512,
+  height: 512,
+  colorDark: "#000",
+  colorLight: "#fff",
+  correctLevel: QRCode.CorrectLevel.H,
+});
+qrcode.clear();
+
 document.querySelector('#login').addEventListener('click', async () => {
   const {user} = await signInWithPopup(auth, provider);
   await update(child(ref(database), `users/${user.uid}`), {
@@ -33,9 +42,149 @@ document.querySelector('#login').addEventListener('click', async () => {
   console.log('success');
   document.querySelector('#login').style.display = 'none';
   const userSnapshot = (await get(child(ref(database), `users/${user.uid}`))).val(); // e, p, t
-  if(p < 1){ // participant
-    console.log(userSnapshot.e, userSnapshot.t); // for data
-  } else { // operator
+  if(userSnapshot.p){ // operator
     // load up web
+    loadReaderApplication();
+  } else { // participant
+    console.log(userSnapshot.e, userSnapshot.t); // for data
+    try {
+      const userdataSnapshot = (await get(child(ref(database), `data/${userSnapshot.e.replaceAll('.','@')}`))).val(); // e, pii
+      console.log("(usercheck) Pass");
+      console.log(userdataSnapshot);
+      
+      qrcode.makeCode(`${window.location.origin}/?data=${encodeURIComponent(`${userSnapshot.e},${userSnapshot.t}`)}`);
+    } catch(error) {
+      console.warn("(usercheck) Fail");
+      updateFeedback("This email is not associated with any registered participants. Please log in with the ucr.edu or gmail account you used to register.");
+    }
   }
 });
+
+load();
+function load(){
+  const params = {};
+  window.location.href
+    .replace(/[^\?]+\?/, "")
+    .split("&")
+    .map((x) => x.split("="))
+    .map(([k, v]) => (params[k] = decodeURIComponent(v)));
+  console.log(params);
+  if(localStorage.getItem('peer') || params.peer){ // in peer mode
+    console.log('i am peripheral');
+    document.querySelector('#login').style.display = 'none';
+
+    const peer = new Peer();
+    if(params.peer) localStorage.setItem('peer', params.peer);
+    updateFeedback("[1/4] connecting ...");
+    peer.on("open", (id) => {
+      updateFeedback("[2/4] connecting ...");
+      const connection = peer.connect(window.localStorage.getItem("peer"));
+      connection.on("open", () => {
+        updateFeedback("[3/4] connected! sending payload now");
+        connection.send(params.data || "null");
+      });
+      connection.on("data", () => connection.close());
+      connection.on("close", () => updateFeedback("[4/4] connection closed; you can close the page now"));
+    });
+    const peerCancel = document.createElement("button");
+    peerCancel.innerText = "Reset";
+    peerCancel.onclick = () => {
+      window.history.pushState(null, "", "?");
+      window.localStorage.removeItem("peer");
+      window.location.reload();
+    };
+    document.body.append(peerCancel);
+  }
+}
+
+function updateFeedback(text){
+  document.querySelector('#feedback').innerText = text;
+}
+function loadReaderApplication(){
+  let currentCard = null;
+  const container = document.createElement("div");
+  const peerReconnect = document.createElement("button");
+  peerReconnect.innerText = "Reconnect";
+  peerReconnect.disabled = true;
+  const openReaderButton = document.createElement("button");
+  openReaderButton.innerText = "Request Serial";
+  openReaderButton.onclick = async () => {
+    // open port (do it once and you can refresh as many times as you want)
+    // reading something
+    try {
+      const decoder = new TextDecoder();
+      const port = await navigator.serial.requestPort();
+      await port.open({ baudRate: 9600 });
+
+      openReaderButton.disabled = true;
+
+      let o = "";
+      if (port.readable) {
+        const reader = port.readable.getReader();
+        updateFeedback("Connected to reader!");
+        try {
+          while (true) {
+            const { value, done } = await reader.read();
+            //console.log("reading");
+            if (done) {
+              // |reader| has been canceled.
+              break;
+            }
+            // Do something with |value|...
+            //decoder.decode(value).then(console.log);
+            // console.log(value);
+            o += decoder.decode(value);
+            // parse output
+            const lines = o.split('\n');
+            o = lines.splice(-1); // leave the last line in buffer
+            for(let line of lines){ // parse lines
+              if(line.startsWith("CARD ID: ")){
+                const matchResult = line.match(/[0-9]+/);
+                if(matchResult && matchResult[0]) currentCard = (matchResult[0]);
+              }
+            }
+          }
+        } finally {
+          reader.releaseLock();
+        }
+        reader.releaseLock();
+        port.close();
+        console.log(o);
+        openReaderButton.disabled = false;
+      }
+    }catch(err){
+      updateFeedback("failed to open port " + err);
+    }
+  };
+
+  const peer = new Peer();
+  peer.on("open", (id) => {
+    if(!navigator.serial){
+      if(location.href.startsWith("http://")) location.replace(location.href.replace('http', 'https'));
+      alert("Serial API not found, make sure you are on a chromium based browser and are using https");
+    }
+
+    qrcode.makeCode(`${window.location.origin}/?peer=${encodeURIComponent(id)}`);
+    peerReconnect.onclick = () => {
+      qrcode.makeCode(`${window.location.origin}/?peer=${encodeURIComponent(id)}`);
+      peerReconnect.disabled = true;
+    };
+    peer.on("connection", (connection) => {
+      console.log(qrcode);
+      qrcode.clear();
+      peerReconnect.disabled = false;
+      // Receive messages
+      connection.on("data", (data) => {
+        console.log("Received", data);
+        connection.send("ok");
+        connection.close();
+      });
+      connection.on("open", () => console.log(" >!!", connection.peer));
+      connection.on("close", () => console.log("<--<", connection.peer));
+      console.log(" >>>", connection.peer);
+    });
+  });
+
+  container.append(openReaderButton, peerReconnect);
+  document.body.append(container);
+}
